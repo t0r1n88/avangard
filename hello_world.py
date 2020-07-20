a4ada6335ef702bc4c351c27a5b5f510236222b7
Eug <vershinina.eugenia@gmail.com>
2020-07-20 17:04:44 +0800

    добавлены еще столбцы кроме вакансий

--- hello_world.py
+++ hello_world.py
@@ -5,13 +5,23 @@ import json
 wb = openpyxl.load_workbook('C:/COPP/avangard/data/vacancies.xlsx')
 sheet = wb['test']
 i = 0
-raw = 1
+raw = 2
 for i in range(0, 30):
     print("http://opendata.trudvsem.ru/api/v1/vacancies/region/0300000000000?offset=" + str(i) + "&limit=100")
     response = requests.get("http://opendata.trudvsem.ru/api/v1/vacancies/region/0300000000000?offset=" + str(i) + "&limit=100")
     print(response.json())
     for element in response.json()['results']['vacancies']:
         sheet['A' + str(raw)] = element['vacancy']['job-name']
+        sheet['B' + str(raw)] = element['vacancy']['source']
+        sheet['C' + str(raw)] = element['vacancy']['salary_min']
+        sheet['D' + str(raw)] = element['vacancy']['salary_max']
+        sheet['E' + str(raw)] = element['vacancy']['employment']
+        sheet['F' + str(raw)] = element['vacancy']['schedule']
+        sheet['G' + str(raw)] = element['vacancy']['category']['specialisation']
+        try:
+            sheet['H' + str(raw)] = element['vacancy']['requirement']['education']
+        except KeyError:
+            print("education")
         raw = raw + 1
         print(raw)
     i = i + 1

bbf9245f4713f9cdfb7eab7cdfb72b1f00e730b0
Eug <vershinina.eugenia@gmail.com>
2020-07-21 13:21:11 +0800

    добавила в trudvsem столбец company

--- hello_world.py
+++ hello_world.py
@@ -14,14 +14,15 @@ for i in range(0, 30):
     for element in response.json()['results']['vacancies']:
         sheet['A' + str(raw)] = element['vacancy']['job-name']
         sheet['B' + str(raw)] = element['vacancy']['source']
-        sheet['C' + str(raw)] = element['vacancy']['salary_min']
-        sheet['D' + str(raw)] = element['vacancy']['salary_max']
-        sheet['E' + str(raw)] = element['vacancy']['employment']
-        sheet['F' + str(raw)] = element['vacancy']['schedule']
-        sheet['G' + str(raw)] = element['vacancy']['category']['specialisation']
-        sheet['H' + str(raw)] = element['vacancy']['duty']
+        sheet['C' + str(raw)] = element['vacancy']['company']['name']
+        sheet['D' + str(raw)] = element['vacancy']['salary_min']
+        sheet['E' + str(raw)] = element['vacancy']['salary_max']
+        sheet['F' + str(raw)] = element['vacancy']['employment']
+        sheet['G' + str(raw)] = element['vacancy']['schedule']
+        sheet['H' + str(raw)] = element['vacancy']['category']['specialisation']
+        sheet['I' + str(raw)] = element['vacancy']['duty']
         try:
-            sheet['I' + str(raw)] = element['vacancy']['requirement']['education']
+            sheet['J' + str(raw)] = element['vacancy']['requirement']['education']
         except KeyError:
             print("education")
         raw = raw + 1
